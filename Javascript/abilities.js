384d871c7811573058a01d8a2095961e39d6ac87
Philip <90202154+PhilipBaravi@users.noreply.github.com>
2021-10-27 16:06:41 +0400

    Abilities

---
+++ Javascript/abilities.js
@@ -0,0 +1,32 @@
+const skillArr = ['https://thumbs.gfycat.com/IgnorantThreadbareGrebe-mobile.mp4', 'https://thumbs.gfycat.com/VictoriousHugeIndianjackal-mobile.mp4', 'https://thumbs.gfycat.com/WeirdImpossibleGerenuk-mobile.mp4', 'https://thumbs.gfycat.com/MemorableSnappyKestrel-mobile.mp4', 'https://thumbs.gfycat.com/ShockedUnsungDwarfrabbit-mobile.mp4', 'https://thumbs.gfycat.com/SardonicFloweryCatfish-mobile.mp4', 'https://thumbs.gfycat.com/JampackedDangerousAmericanlobster-mobile.mp4', 'https://thumbs.gfycat.com/EmptyDecimalBongo-mobile.mp4', 'https://thumbs.gfycat.com/DangerousEssentialKarakul-mobile.mp4', 'https://thumbs.gfycat.com/SnoopyLittleBeaver-mobile.mp4'];
+
+function displaySkill(){
+    document.getElementById("src").src = skillArr[0];
+}
+function displaySkill2(){
+    document.getElementById("src").src = skillArr[1];
+}
+function displaySkill3(){
+    document.getElementById("src").src = skillArr[2];
+}
+function displaySkill4(){
+    document.getElementById("src").src = skillArr[3];
+}
+function displaySkill5(){
+    document.getElementById("src").src = skillArr[4];
+}
+function displaySkill6(){
+    document.getElementById("src").src = skillArr[5];
+}
+function displaySkill7(){
+    document.getElementById("src").src = skillArr[6];
+}
+function displaySkill8(){
+    document.getElementById("src").src = skillArr[7];
+}
+function displaySkill9(){
+    document.getElementById("src").src = skillArr[8];
+}
+function displaySkill10(){
+    document.getElementById("src").src = skillArr[9];
+}
